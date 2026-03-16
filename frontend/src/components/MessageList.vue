4c3b4b4a91e5fe45fcc15ed7e53aaaecb5daf5fe
v <v@pwns.me>
2026-03-16 16:15:11 -0700

    fix(frontend): Messages references

--- frontend/src/components/MessageList.vue
+++ frontend/src/components/MessageList.vue
@@ -5,17 +5,18 @@ const props = defineProps<{
   messages: ChatMessage[]
 }>()
 
+console.log(props.messages);
 </script>
 
 <template>
     <div class="w-full max-w-xl rounded-md p-4 mb-4 h-64 overflow-y-auto chatbox">
-      <div v-for="(msg, index) in messages" :key="index" class="mb-2">
+      <div v-for="(msg, index) in props.messages" :key="index" class="mb-2">
         <div class="message-text px-3 py-2 rounded-md"
              :class="`message-${msg.sender}`">
-          {{ msg.text }}
+          {{ msg.content }}
         </div>
         <div v-if="msg.sender !== 'system'" class="message-meta">
-          <span class="message-identity">{{ msg.identity }}</span>
+          <span class="message-identity">{{ msg.identity.display_name }}</span>
           <span class="message-actions">
             <!-- TODO: copy button etc go here -->
           </span>
